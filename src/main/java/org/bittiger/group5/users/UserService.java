ced62234412b753334dbd46af5958bc7b76168ff
Yuzhou Xu <milegedan@gmail.com>
2016-07-28 20:00:16 -0400

    resubmit

--- src/main/java/org/bittiger/group5/users/UserService.java
+++
@@ -1,7 +0,0 @@
-package org.bittiger.group5.users;
-
-/**
- * Created by Ian on 7/24/2016.
- */
-public class UserService {
-}
--- src/main/java/org/bittiger/group5/util/MiniAppConstants.java
+++
@@ -1,9 +0,0 @@
-package org.bittiger.group5.util;
-
-public class MiniAppConstants {
-	public static final String AUTH_TOKEN = "b5a457a3f48ae95df72b6c5e82f73bfba7684fb5";
-	public static final String SEARCH_ENT_APPS_URL = "https://data.42matters.com/api/v2.0/ios/apps/search.json?q=entertainment&limit=10%20&access_token=b5a457a3f48ae95df72b6c5e82f73bfba7684fb5";
-	public static final String SEARCH_ACTION_APPS_URL = "https://data.42matters.com/api/v2.0/ios/apps/search.json?q=action&limit=10%20&access_token=b5a457a3f48ae95df72b6c5e82f73bfba7684fb5";
-	public static final String SEARCH_SOCIAL_APPS_URL = "https://data.42matters.com/api/v2.0/ios/apps/search.json?q=social%20networking&limit=10%20&access_token=b5a457a3f48ae95df72b6c5e82f73bfba7684fb5";
-	public static final String SEARCH_TOP_APPS_URL = "https://data.42matters.com/api/v2.0/ios/apps/query.json?access_token=b5a457a3f48ae95df72b6c5e82f73bfba7684fb5";
-}
